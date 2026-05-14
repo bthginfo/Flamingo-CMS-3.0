0aebfc66a466020d1b8d9affea85e4da212f1035
von Ingelheim <vonin-ju@de001.itgr.net>
2026-05-14 18:35:31 +0200

    merge CRM into marketing app under /crm, remove standalone crm app

--- apps/crm/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ['@flamingo/db', '@flamingo/auth'],
-};
-
-module.exports = nextConfig;
